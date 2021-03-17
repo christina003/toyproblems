3f65e4d38819f49f1ece59b4dfc16b2298c261ac
Christina Santos <christina.santos823@gmail.com>
2021-03-17 13:48:13 -0700

    completed BST traversal

---
+++ BSTtraversal.js
@@ -0,0 +1,32 @@
+/*Write three functions that take in a Binary Search Tree and an empty array, traverse the BST, add its nodes' values to the input array and return that array. The three functions should traverse the BST using the in-order, pre-order, and post-order tree-traversal techniques, respectively.
+
+in order: left, parent, right
+preorder: root, left, right
+post order: left, right, root*/
+
+function inOrderTraverse(tree, array) {
+  if (tree !== null) {
+    inOrderTraverse(tree.left, array);
+    array.push(tree.value);
+    inOrderTraverse(tree.right, array);
+  }
+  return array;
+}
+
+function preOrderTraverse(tree, array) {
+  if (tree !== null) {
+    array.push(tree.value);
+    preOrderTraverse(tree.left, array);
+    preOrderTraverse(tree.right, array);
+  }
+  return array;
+}
+
+function postOrderTraverse(tree, array) {
+  if (tree !== null) {
+    postOrderTraverse(tree.left, array);
+    postOrderTraverse(tree.right, array);
+    array.push(tree.value);
+  }
+  return array;
+}
