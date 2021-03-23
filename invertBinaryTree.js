ecff25f4b0b3d1211c0daeb2b5c38e405a12c73d
Christina Santos <christina.santos823@gmail.com>
2021-03-22 18:57:53 -0700

    added notes

--- invertBinaryTree.js
+++ invertBinaryTree.js
@@ -1,12 +1,18 @@
 /*write a function that takes in a binary tree and inverts it. In other words, the function should swap every left node in the tree for its corresponding right node */
 
 function invertBinaryTree(tree) {
+  //base case
+  //if no tree then return
   if (!tree) {
     return;
   } else {
+    //create temp variable to hold left node
     let tmp = tree.left;
+    //set left to equal right
     tree.left = tree.right;
+    //set right to equal left
     tree.right = tmp;
+    //call function on left and right
     invertBinaryTree(tree.left);
     invertBinaryTree(tree.right);
   }
