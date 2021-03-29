517f55113d567f8441332e9e05fd9ffadcc52adc
Christina Santos <christina.santos823@gmail.com>
2021-03-28 18:17:51 -0700

    review palindrome check

--- palindromeCheck.js
+++ palindromeCheck.js
@@ -1,5 +1,7 @@
 /* write a function that takes in a non-empty string and that returns a boolean representing whether the string is a palindrome. */
 
+//create another method without reverse
+
 function isPalindrome(string) {
   const reverseString = string.split("").reverse().join("");
 
