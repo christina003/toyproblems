6f30c34990e545d601ac582db01ac859a5fa06ca
Christina Santos <christina.santos823@gmail.com>
2021-04-20 17:55:57 -0700

    completed array of products

---
+++ arrayOfProducts.js
@@ -0,0 +1,20 @@
+function arrayOfProducts(array) {
+  let result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    let j = 0;
+    let product = 1;
+    while (j < array.length) {
+      if (j !== i) {
+        product = product * array[j];
+        j++;
+      } else {
+        j++;
+      }
+    }
+
+    result[i] = product;
+  }
+
+  return result;
+}
